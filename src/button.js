515da72b32fe0338ec4973895815a6928960967a
M.C. van den Boogaart <scola@kvdb.name>
2016-06-08 11:43:40 +0200

    Add helper methods to set button styles

--- src/button.js
+++ src/button.js
@@ -11,19 +11,27 @@ export default class Button {
       .styles({
         'background': '#FFF',
         'border': 0,
-        'border-left': '1px none #CCC',
+        'border-left': '1px solid #CCC',
         'border-top': '1px solid #CCC',
         'cursor': 'pointer',
-        'float': 'left',
         'height': '3em',
         'line-height': '3em',
         'padding': 0,
-        'text-align': 'center',
-        'width': '100%'
+        'text-align': 'center'
       });
   }
 
   node() {
     return this.outer.node();
+  }
+
+  top() {
+    this.outer.style('border-top-color', '#FFF');
+    return this;
+  }
+
+  left() {
+    this.outer.style('border-left-style', 'none');
+    return this;
   }
 }
